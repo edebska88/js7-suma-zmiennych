cc2e2aa37a006e2a894396a46b41ff3ea07a35e8
Emilia Debska <emiliad88@gmail.com>
2021-11-21 23:41:35 +0100

    suma zmiennych

---
+++ js/main.js
@@ -0,0 +1,8 @@
+let firstNumber = 5
+let secondNumber = 2
+
+function add(num1, num2) {
+    let suma = firstNumber + secondNumber;
+    return suma;
+}
+console.log(add(firstNumber, secondNumber));
