37dc220e7f7926c4e6314ae97133fcf0378f447f
Emilia Debska <emiliad88@gmail.com>
2021-11-25 09:52:12 +0100

    poprawiona zmienna suma

--- js/main.js
+++ js/main.js
@@ -2,7 +2,10 @@ let firstNumber = 5
 let secondNumber = 2
 
 function add(num1, num2) {
-    let suma = firstNumber + secondNumber;
+    let suma = num1 + num2;
     return suma;
 }
+
 console.log(add(firstNumber, secondNumber));
+console.log(add(2, 3));
+console.log(add(20, 5));
